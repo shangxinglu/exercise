86b59e2b8dd40d5ac2a8a3bf5d686044e14f0961
shangxinglu <2609209328@qq.com>
2021-08-01 16:28:03 +0800

    toy-js

---
+++ toy-javascript/lexicalParse.js
@@ -0,0 +1,125 @@
+
+
+const regexpObj = {
+    Program: 'WhiteSpace|LineTerminator|Comment|Token',
+    WhiteSpace: / /,
+    LineTerminator: /\r|\n/,
+    Comment: /\/\/[^\n]*|\/\*[\s\S]*?\*\//,
+    Token: 'Literal|Keyword|Identifier|Punctuator',
+    Literal: 'BooleanLiteral|NumberLiteral|StringLiteral|NullLiteral',
+    BooleanLiteral: /true|false/,
+    NumberLiteral: /(?:[0-9]|[1-9][0-9]*)(?:\.[0-9]+)?/,
+    StringLiteral: /'(?:[^'])*?'|"(?:[^"])*?"/,
+    NullLiteral: /null/,
+    Keyword: /var|let|const|if|else[\s]+if|else|switch|case|return|for|while|break|continue/,
+    Identifier: /[A-Za-z$_][A-Za-z0-9$_]*/,
+    Punctuator: /\(|\)|\{|\}|;|\:|\.|\[|\]|\+|\-|\*|\/|\+\+|\-\-|\=|\=\=|\=\=\=|\>|\<|\,|\!/,
+}
+
+export function genRegexp(entry) {
+
+    // 字符串分割，遍历查找对象的属性，
+    // 如果是正则替换，
+    // 如果是字符串，重复以上步骤
+    function mergeRegexp(entry) {
+        let regepxStr = '';
+        let current = regexpObj[entry], matchArr = [];
+
+        if (typeof current === 'string') {
+
+            for (let item of current.split('|')) {
+                let child = regexpObj[item];
+                if (child instanceof RegExp) {
+                    matchArr.push(`(?<${item}>${child.source})`);
+                }
+
+                if (typeof child === 'string') {
+                    matchArr.push(mergeRegexp(item));
+                }
+            }
+
+            regepxStr = matchArr.join('|');
+
+        }
+
+        return regepxStr;
+    }
+
+    const regexp = new RegExp(mergeRegexp(entry), 'g');
+
+    return regexp;
+
+}
+
+
+
+
+
+const regexp = genRegexp('Program');
+
+export function* lexicalParse(code){
+    let match;
+    while (match = regexp.exec(code)) {
+        const { groups } = match;
+        const [value] = match;
+
+    
+        if (groups.WhiteSpace) {
+            // yield {
+            //     type:'WhiteSpace',
+            //     value,
+            // };
+        } else if (groups.LineTerminator) {
+            // yield {
+            //     type:'LineTerminator',
+            //     value,
+            // };
+    
+        } else if (groups.Comment) {
+            // yield {
+            //     type:'Comment',
+            //     value,
+            // };
+    
+        } else if (groups.NumberLiteral) {
+            yield {
+                type:'NumberLiteral',
+                value,
+            };
+    
+        } else if (groups.StringLiteral) {
+            yield {
+                type:'StringLiteral',
+                value,
+            };
+    
+        } else if (groups.NullLiteral) {
+            yield {
+                type:'NullLiteral',
+                value,
+            };
+    
+        }else if (groups.Identifier) {
+            yield {
+                type:'Identifier',
+                value,
+            };
+    
+        } else if (groups.Keyword) {
+            yield {
+                type:value,
+            };
+    
+        }  else if (groups.Punctuator) {
+            yield {
+                type:value,
+            };
+    
+        }
+    }
+
+    yield {
+        type:'EOF'
+    }
+}
+
---
+++ toy-javascript/syntaxParse.js
@@ -0,0 +1,262 @@
+
+import { lexicalParse } from './lexicalParse.js'
+
+const SYNTAX = {
+    Program: [
+        ['StatementList', 'EOF'],
+    ],
+
+    StatementList: [
+        ['Statement'],
+        ['StatementList', 'Statement'],
+    ],
+
+    Statement: [
+        ['ExpressionStatement'],
+        ['IfStatement'],
+        ['ForStatement'],
+        ['WhileStatement'],
+        ['VarDeclaration'],
+        ['FunctionDeclaration'],
+    ],
+
+    ExpressionStatement: [
+        ['Expression', ';'],
+    ],
+    Expression: [
+        ['AdditionExpression'],
+    ],
+
+    AdditionExpression: [
+        ['MultiplicationExpression'],
+        ['AdditionExpression', '+', 'MultiplicationExpression'],
+        ['AdditionExpression', '-', 'MultiplicationExpression'],
+    ],
+
+
+    MultiplicationExpression: [
+        ['PrimaryExpression'],
+        ['MultiplicationExpression', '*', 'PrimaryExpression'],
+        ['MultiplicationExpression', '/', 'PrimaryExpression'],
+    ],
+    PrimaryExpression: [
+        ['(', 'Expression', ')'],
+        ['Literal'],
+        ['Identifier'],
+    ],
+    Literal: [
+        ['NullIteral'],
+        ['BooleanIteral'],
+        ['NumerIteral'],
+        ["StringIteral"],
+    ],
+
+    IfStatement: [
+        ['if', '(', 'Expression', ')', 'Statement'],
+    ],
+
+    VarDeclaration: [
+        ['var', 'Identifier', ';'],
+        ['let', 'Identifier', ';'],
+    ],
+}
+
+const hashTable = {};
+
+const start = {
+    Program: {},
+}
+
+/**
+ * @description 对语法结构进行展开
+ */
+function closure(state) {
+    const hash = JSON.stringify(state);
+    hashTable[hash] = state;
+    const queue = [];
+    let current, currentRule;
+    for (let item of Reflect.ownKeys(state)) {
+        if (['reduceType', 'reduceLength'].includes(item)) continue;
+        queue.push(item);
+    }
+
+
+    while (queue.length) {
+        current = queue.shift();
+        currentRule = SYNTAX[current];
+        if (!currentRule) continue;
+
+        // debugger
+        for (let ruleArr of currentRule) {
+            // const [rule] = ruleArr;
+            if (!state[ruleArr[0]]) {
+                queue.push(ruleArr[0]);
+            }
+
+            let currentState = state;
+
+            for (let rule of ruleArr) {
+                if (!currentState[rule]) {
+                    currentState[rule] = {};
+                }
+                currentState = currentState[rule];
+            }
+
+            currentState.reduceType = current;
+            currentState.reduceLength = ruleArr.length;
+
+        }
+    }
+
+    const stateRule = Reflect.ownKeys(state);
+    for (let item of stateRule) {
+        if (['reduceType', 'reduceLength'].includes(item)) continue;
+
+        let rule = state[item];
+        const hash = JSON.stringify(rule);
+        if (hashTable[hash]) {
+            rule = hashTable[hash];
+        } else {
+            closure(rule);
+        }
+    }
+
+
+}
+
+closure(start);
+
+
+
+export function syntaxParse(code) {
+    const stack = [start];
+    const tokenStack = [];
+    function reduce() {
+        let current = stack[stack.length - 1];
+        if (current.reduceType) {
+            const children = [];
+            for (let i = 0; i < current.reduceLength; i++) {
+                stack.pop();
+                children.push(tokenStack.pop());
+
+            }
+
+            const token = {
+                type: current.reduceType,
+                children: children.reverse(),
+            }
+
+            return token;
+        } else {
+            // debugger
+        }
+    }
+
+    function shift(token) {
+        let current = stack[stack.length - 1];
+
+        if (Reflect.ownKeys(current).includes(token.type)) {
+            stack.push(current[token.type]);
+            tokenStack.push(token);
+        } else {
+            shift(reduce());
+            shift(token);
+        }
+
+    }
+    for (let token of lexicalParse(code)) {
+        shift(token);
+
+    }
+
+
+    return reduce();
+}
+
+
+const execObj = {
+    Program(node) {
+        const [child] = node.children;
+        exec(child);
+    },
+    StatementList(node) {
+        const { children } = node,
+            { length } = children;
+        switch (length) {
+            case 1:
+                return exec(children[0]);
+                break;
+
+            case 2:
+                exec(children[0]);
+                return exec(children[1]);
+                break;
+        }
+    },
+    Statement(node){
+        const {children} = node;
+        return exec(children[0]);
+        
+    },
+
+    VarDeclaration(node){
+        const {children} = node;
+        console.log('VarDeclaration',children[1].value);
+    }
+}
+
+function exec(node) {
+    return execObj?.[node.type]?.(node);
+}
+
+
+const code = `
+    let a;
+    let b;
+`;
+
+const tree = syntaxParse(code);
+
+exec(tree);
+// const code = `
+
+// // 注释
+// /*
+// 注释
+// */
+// let a = 'str';
+// send(connect) {
+//     return new Promise((resolve, reject) => {
+
+//         if (connect) {
+//             connect.write(this.toString());
+//         } else {
+//             connect = net.createConnection({
+//                 port: this.port,
+//                 host: this.host,
+//             }, () => {
+//                 console.log('createConnect');
+//                 connect.write(this.toString());
+
+//             });
+//         }
+
+//         connect.on('data', chunk => {
+//             console.log('data', chunk.toString());
+//             const response = new Response;
+
+//             response.receiver(chunk.toString());
+//             resolve(response.getResponse());
+//         })
+
+//         connect.on('error', error => {
+//             console.log('error', error);
+//             reject(error);
+//             connect.end();
+//         })
+//     })
+
+
+// }
+// `;
+
